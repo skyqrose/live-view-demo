20c4d93546f9efed6abd701c57b09e757a3ff73e
Cora Grant <394835+x@users.noreply.github.com>
2019-12-11 14:09:34 -0500

    Add live map with React

--- assets/js/components/Route.jsx
+++ assets/js/components/Route.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
 
 export default (props) => {
   const [route, setRoute] = useState(props);
@@ -30,6 +31,16 @@ export default (props) => {
       <h2>{name}</h2>
 
       {!isOnline && <p>Error getting data. Reconnecting...</p>}
+
+      <Map center={[42.5, -71.5]} zoom={8}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        {vehicles.map(vehicle => (
+          <Marker position={[vehicle.latitude, vehicle.longitude]}></Marker>
+        ))}
+      </Map>
 
       <table border="1" cellPadding="8" cellSpacing="0">
         <tbody>
